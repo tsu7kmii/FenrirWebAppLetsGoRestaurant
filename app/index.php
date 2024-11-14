d952de9ea469136dc70617fb50a715ccef27d1a4
kmiibfw <kmiibfw@gmail.com>
2024-11-14 18:34:29 +0900

    [fix] docekrホスト時のデフォルトindexから検索ページにリダイレクトへ変更

--- app/index.php
+++ app/index.php
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<html lang="ja">
-<?php $title = "Let's Go Restaurant - トップページ"; ?>
-<?php include_once(__DIR__.'/views/template/header.php'); ?>
-<body>
-    <h1>Let's Go Restaurant</h1>
-    <p>フェンリル2026新卒採用 選考課題</p>
-
-    <h2>利用を開始する</h2>
-    <div>
-        <ul>
-            <li><a href="./views/search.php">検索する</a></li>
-        </ul>
-    </div>
-
-    <?php include_once(__DIR__ .'/views/template/footer.html'); ?>
-
-</body>
-</html>
+<?php
+header('Location: /views/search.php');
+exit();
+?>
